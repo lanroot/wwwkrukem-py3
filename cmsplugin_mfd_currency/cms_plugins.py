b413e3a311e039de96d77cf74f42527601205fca
lanroot <lan@kmr.ru>
2017-04-27 10:19:31 +0700

    Except timout in cmsplugin_mfd_currency

--- cmsplugin_mfd_currency/cms_plugins.py
+++ cmsplugin_mfd_currency/cms_plugins.py
@@ -1,4 +1,5 @@
 from urllib.request import urlopen
+from socket import timeout
 from cms.plugin_pool import plugin_pool
 from cms.plugin_base import CMSPluginBase
 from django.utils.translation import ugettext_lazy as _
@@ -11,6 +12,7 @@ class MfdCurrencyPlugin(CMSPluginBase):
     render_template = "cmsplugin_mfd_currency/mfd_currency.html"
     
     def render(self, context, instance, placeholder):
+        urltimeout = 3
         request = context['request']
 
         cache_key_mfdcurrency = 'mfdcurrency_result'
@@ -26,20 +28,33 @@ class MfdCurrencyPlugin(CMSPluginBase):
         mfdimage = cache.get(cache_key_mfdimage)
 
         if not mfdcurrency:
-            mfdcurrency_response = urlopen(mfdcurrency_url)
-            mfdcurrency = mfdcurrency_response.read()
+            try:
+                mfdcurrency_response = urlopen(mfdcurrency_url, timeout=urltimeout)
+            except timeout:
+                mfdcurrency = "no data"
+            else:
+                mfdcurrency = mfdcurrency_response.read()
             cache.set(cache_key_mfdcurrency, mfdcurrency, getattr(settings, 'MFD_CURRENCY_CACHE_DURATION', 60*60))
 
         if not mfdtable:
-            mfdtable_response = urlopen(mfdtable_url)
-            mfdtable = mfdtable_response.read()
+            try:
+                mfdtable_response = urlopen(mfdtable_url, timeout=urltimeout)
+            except timeout:
+                mfdtable = "no data"
+            else:
+                mfdtable = mfdtable_response.read()
             cache.set(cache_key_mfdtable, mfdtable, getattr(settings, 'MFD_TABLE_CACHE_DURATION', 60*60))
 
         if not mfdimage:
-            mfdimage = urlopen(mfdimage_url).read()
-            f = open("media/downloads/mfdimage.png", "wb")
-            f.write(mfdimage)
-            f.close()
+            try:
+                mfdimage_response = urlopen(mfdimage_url, timeout=urltimeout)
+            except timeout:
+                mfdimage = "no data"
+            else:
+                mfdimage = mfdimage_response.read()
+                f = open("media/downloads/mfdimage.png", "wb")
+                f.write(mfdimage)
+                f.close()
             cache.set(cache_key_mfdimage, 'Exist', getattr(settings, 'MFD_IMAGE_CACHE_DYRATION', 60*5))
 
         context.update({
