2ceffd51603a082bbdb16715bd1a4fdfc83f7b31
lanroot <lan@kmr.ru>
2019-04-11 09:40:19 +0700

    API OpenWeatherMap not return wind direction now

--- cmsplugin_openweathermap/cms_plugins.py
+++ cmsplugin_openweathermap/cms_plugins.py
@@ -35,7 +35,7 @@ class OpenWeatherMapPlugin(CMSPluginBase):
             else:
                 weather_info_json = openweathermap_response.read().decode('utf-8')
                 weather_info = json.loads(weather_info_json)
-                weather_info['wind']['direction'] = degToCompass(weather_info['wind']['deg'])
+#                weather_info['wind']['direction'] = degToCompass(weather_info['wind']['deg'])
             cache.set(cache_key, weather_info, getattr(settings, 'OPENWEATHERMAP_CACHE_DURATION', 60*60))
             
         context.update({
